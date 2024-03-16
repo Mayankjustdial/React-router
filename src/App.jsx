3e29d66802a435493be5fb260003226e2781bb70
Mayank <mayankghz225@gmail.com>
2024-03-16 17:23:42 +0530

    No match Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Home } from "./components/Home";
 import { About } from "./components/About";
 import { Navbar } from "./components/Navbar";
 import OrderSummary from "./components/OrderSummary";
+import { NoMatchRoute } from "./components/NoMatchRoute";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/order-summary" element={<OrderSummary />} />
+        <Route path="*" element={<NoMatchRoute />} />
       </Routes>
     </>
   );
