892d42b3ed75a0b698682723a8c4bb537a622acb
Mayank <mayankghz225@gmail.com>
2024-03-17 15:20:26 +0530

    Dynnamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import { NoMatchRoute } from "./components/NoMatchRoute";
 import { Products } from "./components/Products";
 import { FeaturedProducts } from "./components/FeaturedProducts";
 import { NewProducts } from "./components/NewProducts";
+import Users from "./components/Users";
+import UserDetails from "./components/UserDetails";
+import Admin from "./components/Admin";
+
 function App() {
   return (
     <>
@@ -21,6 +25,11 @@ function App() {
           <Route path="featured" element={<FeaturedProducts />} />
           <Route path="new" element={<NewProducts />} />
         </Route>
+        <Route path="/users" element={<Users />}>
+          <Route path=":userId" element={<UserDetails />} />
+          <Route path="admin" element={<Admin />} />
+        </Route>
+
         <Route path="*" element={<NoMatchRoute />} />
       </Routes>
     </>
---
+++ src/components/Admin.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Admin = () => {
+  return <div>Admin details page</div>;
+};
+
+export default Admin;
