f8724ed8a1ee44c658cd3ab57369eaaf7eaae3a6
Mayank <mayankghz225@gmail.com>
2024-03-17 15:24:37 +0530

    URL params

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const UserDetails = () => {
-  return <div>details about users</div>;
+  const params = useParams();
+  const userId = params.userId;
+  return <div>details about user with id :{userId}</div>;
 };
 
 export default UserDetails;
